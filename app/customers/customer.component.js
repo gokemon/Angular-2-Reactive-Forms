d595fc37a2b7b0113381cbda0f2e60aecb708627
gokemon <michael.of.gokey@gmail.com>
2017-05-20 09:48:40 -0500

    m3

--- app/customers/customer.component.js
+++ app/customers/customer.component.js
@@ -8,15 +8,33 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var forms_1 = require("@angular/forms");
 var customer_1 = require("./customer");
 var CustomerComponent = (function () {
-    function CustomerComponent() {
+    function CustomerComponent(fb) {
+        this.fb = fb;
         this.customer = new customer_1.Customer();
     }
-    CustomerComponent.prototype.save = function (customerForm) {
-        console.log(customerForm.form);
-        console.log('Saved: ' + JSON.stringify(customerForm.value));
+    CustomerComponent.prototype.ngOnInit = function () {
+        this.customerForm = this.fb.group({
+            firstName: '',
+            lastName: '',
+            email: '',
+            sendCatalog: true
+        });
+    };
+    CustomerComponent.prototype.populateTestData = function () {
+        this.customerForm.patchValue({
+            firstName: 'Jack',
+            lastName: 'Harkness',
+            sendCatalog: false
+        });
+    };
+    CustomerComponent.prototype.save = function () {
+        console.log(this.customerForm);
+        console.log('Saved: ' + JSON.stringify(this.customerForm.value));
     };
     return CustomerComponent;
 }());
@@ -25,7 +43,7 @@ CustomerComponent = __decorate([
         selector: 'my-signup',
         templateUrl: './app/customers/customer.component.html'
     }),
-    __metadata("design:paramtypes", [])
+    __metadata("design:paramtypes", [forms_1.FormBuilder])
 ], CustomerComponent);
 exports.CustomerComponent = CustomerComponent;
 //# sourceMappingURL=customer.component.js.map
